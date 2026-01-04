69d4bc92e7c9eeb81132c3d217f04e4b4ace3580
rizwanyim <rizwanyim@gmail.com>
2026-01-05 00:42:16 +0800

    Initial commit for new repository

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  // Gunakan '/' untuk paparan localhost dan Vercel
   base: '/',
   build: {
     outDir: 'dist',
